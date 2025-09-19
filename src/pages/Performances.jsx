588925bb9a326f783056f5e79dbc00a80ada1972
mxou <maximilien.dlrm@gmail.com>
2025-09-19 13:27:32 +0200

    perfs page debut

--- src/pages/Performances.jsx
+++ src/pages/Performances.jsx
@@ -1,5 +1,8 @@
 import Navbar from "../components/Navbar";
 import localforage from "localforage";
+import Header from "../components/Header";
+import { useState } from "preact/hooks";
+
 import styles from "./styles/Performances.module.css";
 
 export default function Performances() {
@@ -8,9 +11,38 @@ export default function Performances() {
   //       if (data) setPrograms(data);
   //     });
   //   }, []);
+
+  const [categ, setCateg] = useState(null);
+
+  const handleClickCateg = () => {
+    setCateg(name);
+  };
   return (
     <main>
-      <h1>Perfs</h1>
+      <Header HeaderTitle={"Performances"} Return={true} />
+      <div className={styles.select_container}>
+        <label for="exo_select">Choisir un exercice</label>
+        <select>
+          <option id="exo_select" value="">
+            Choisir un exercice
+          </option>
+          <option value="developpe_couche_barre">Développé couché barre</option>
+          <option value="curl_pupitre">Curls puprite</option>
+          <option value="tractions">Tractions</option>
+        </select>
+      </div>
+      <div className={styles.exo_categ}>
+        <p className={`${styles.categ} ${categ === "poids" ? styles.active : ""}`} onClick={() => handleClickCateg("poids")}>
+          Poids
+        </p>
+        <p className={`${styles.categ} ${categ === "series" ? styles.active : ""}`} onClick={() => handleClickCateg("series")}>
+          Séries
+        </p>
+        <p className={`${styles.categ} ${categ === "reps" ? styles.active : ""}`} onClick={() => handleClickCateg("reps")}>
+          Répétitions
+        </p>
+      </div>
+      <canvas></canvas>
       <Navbar />
     </main>
   );
